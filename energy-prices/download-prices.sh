13f6b93de28159cc36dbed26faf9427629f31cc0
jimo42 <mothejzik@email.cz>
2025-05-25 01:38:56 +0200

    minor update

--- energy-prices/download-prices.sh
+++ energy-prices/download-prices.sh
@@ -11,7 +11,8 @@ get_file(){
 	URL="https://www.ote-cr.cz/cs/kratkodobe-trhy/elektrina/denni-trh?date=$DESIRED_DATE"
 	curl -s "$URL" > "$HTML"
 	if [ $? -eq 0 ] ; then echo -n " File downloaded." ; else echo -n " Error while downloading the file."; fi
-	DATE_READ=`cat $HTML | grep serverDataUrl | grep 'kratkodobe-trhy/elektrina/denni-trh/' | cut -d\= -f3 | cut -d\" -f1`
+#	DATE_READ=`cat $HTML | grep serverDataUrl | grep 'kratkodobe-trhy/elektrina/denni-trh/' | cut -d\= -f3 | cut -d\" -f1`
+	DATE_READ=`cat $HTML | grep serverDataUrl | grep 'kratkodobe-trhy/elektrina/denni-trh/' | cut -d\= -f3 | cut -d\" -f1 | cut -d\& -f1`
 	if [ "$DESIRED_DATE" != "$DATE_READ" ] ; then echo -n " Dates don't match."; exit 1; fi
 	cat "$HTML" | pup 'table.report_table tbody tr json{}' | jq -r '.[] | [.children[0].text, .children[1].text] | join(";")' | grep ^[0-9] > $CSV
 	if [ ! -s "$DATE"".csv" ] ; then echo -n " CSV has zero size, will be re-attempted during next execution."; fi
@@ -32,11 +33,9 @@ fi
 
 DATE=`date "+%Y-%m-%d" --date "tomorrow"`
 if [ -f "$DATE"".csv" ]; then
-    echo -n " Tomorrow's file exist."
+    echo " Tomorrow's file exist."
 else
-    echo -n " Tomorrow's file does not exist."
+    echo " Tomorrow's file does not exist."
     if [ $(date +"%H") -ge 14 ] ; then get_file "$DATE" ; fi
 fi
-
-echo
 
