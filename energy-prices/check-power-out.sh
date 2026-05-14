9139aeb8750ca71e5580535d886f3057b270421f
jimo42 <mothejzik@email.cz>
2026-05-14 23:04:26 +0200

    update

--- energy-prices/check-power-out.sh
+++ energy-prices/check-power-out.sh
@@ -5,7 +5,7 @@ cd $SCRIPT_DIR
 PYSCRIPTS_DIR="$SCRIPT_DIR/../goodwe"
 
 #Threshold, pri krerem se nam nevyplati prodavat
-THRESHOLD="20"
+THRESHOLD="22"
 
 enable_grid_output(){
 	if [[ `$PYSCRIPTS_DIR/manage_goodwe.py read_export_limit | grep ENABLED | wc -l` -eq 1 ]] ; then 
@@ -14,6 +14,9 @@ enable_grid_output(){
 #	if  `$PYSCRIPTS_DIR/goodwe_export_limit_read2.py | grep 1 >/dev/null` ; then
 #		$PYSCRIPTS_DIR/goodwe_export_limit_disable.py
 #	fi
+
+	# temporary workaroud for water heating when battery is not functional
+	# $SCRIPT_DIR/../relatka/vypni_bojler_naplno.sh
 }
 
 disable_grid_output(){
@@ -25,6 +28,9 @@ disable_grid_output(){
 #		$PYSCRIPTS_DIR/goodwe_export_limit_enable.py
 #		$PYSCRIPTS_DIR/goodwe_export_limit_set_to_zero.py
 #	fi
+
+	# temporary workaroud for water heating when battery is not functional
+	# $SCRIPT_DIR/../relatka/pust_bojler_na_2a3.sh
 }
 
 
@@ -32,15 +38,26 @@ DATE_NOW=$(date "+%Y-%m-%d")
 HOUR_NOW=$(date +"%H:%M")
 #[[ $HOUR_NOW -eq 0 ]] && HOUR_NOW=24
 
-PRICE_NOW=$(cat "$DATE_NOW".csv | grep ^"$HOUR_NOW"';' | cut -d\; -f2 | tr \, \. )
+# if primary file does not exist, use the secondary one
+PRICES="$DATE_NOW".csv
+if [ ! -f "$PRICES" ] || [ "$(stat -c %s "$PRICES" 2>/dev/null)" -lt 1024 ]; then
+	PRICES="$DATE_NOW"_check.csv
+	NOTIF=" (secondary prices file used)"
+	if [ ! -f "$PRICES" ] || [ "$(stat -c %s "$PRICES" 2>/dev/null)" -lt 1024 ]; then
+		echo "Both primary and secondary file with prices do not exist or are corrupted, exiting."
+		exit 1
+	fi
+fi
+
+PRICE_NOW=$(cat "$PRICES" | grep ^"$HOUR_NOW"';' | cut -d\; -f2 | tr \, \. )
 
 echo -n "`date` - "
 
 if (( $(echo "$PRICE_NOW < $THRESHOLD" | bc -l) )); then
-	echo "Current price $PRICE_NOW is lower than threshold, we should turn off the output to grid."
+	echo "Current price $PRICE_NOW is lower than threshold, we should turn off the output to grid""$NOTIF""."
 	disable_grid_output
 else
-	echo "Current price $PRICE_NOW higher than threshold, we can enable output to grid."
+	echo "Current price $PRICE_NOW higher than threshold, we can enable output to grid""$NOTIF""."
 	enable_grid_output
 fi
 
--- energy-prices/download-prices.sh
+++ energy-prices/download-prices.sh
@@ -13,13 +13,16 @@ get_file(){
 	if [ $? -eq 0 ] ; then echo -n " File downloaded." ; else echo -n " Error while downloading the file."; fi
 #	DATE_READ=`cat $HTML | grep serverDataUrl | grep 'kratkodobe-trhy/elektrina/denni-trh/' | cut -d\= -f3 | cut -d\" -f1`
 	DATE_READ=`cat $HTML | grep serverDataUrl | grep 'kratkodobe-trhy/elektrina/denni-trh/' | cut -d\= -f3 | cut -d\" -f1 | cut -d\& -f1`
-	if [ "$DESIRED_DATE" != "$DATE_READ" ] ; then echo -n " Dates don't match."; exit 1; fi
-	cat "$HTML" | pup 'table.report_table tbody tr json{}' | jq -r '.[] | [.children[0].text, .children[1].text] | join(";")' | grep ^[0-9]  \
-	| while read L; do echo `echo $L | cut -d\- -f1`\;`echo $L | cut -d\; -f2`; done     > $CSV
-	if [ ! -s "$DATE"".csv" ] ; then echo -n " CSV has zero size, will be re-attempted during next execution."; fi
+	if [ "$DESIRED_DATE" == "$DATE_READ" ] ; then
+		cat "$HTML" | pup 'table.report_table tbody tr json{}' | jq -r '.[] | [.children[0].text, .children[1].text] | join(";")' | grep ^[0-9]  \
+		| while read L; do echo `echo $L | cut -d\- -f1`\;`echo $L | cut -d\; -f2`; done     > $CSV
+		if [ ! -s "$DATE"".csv" ] ; then echo -n " CSV has zero size, will be re-attempted during next execution."; fi
+	else
+		echo -n " Dates don't match."
+	fi
 
 # check from second source
-	./getPricesFromENTSOE.py "$DESIRED_DATE"
+	[ ! -e "$DESIRED_DATE"_check.csv ] && ./getPricesFromENTSOE.py "$DESIRED_DATE"
 	diff $CSV "$DESIRED_DATE"_check.csv
 	if [ $? -ne 0 ] ; then echo -n " CSVs from two sources differ!"; else echo " CSVs from both sources are the same, deleting the check one."; rm "$DESIRED_DATE"_check.csv; fi
 }
