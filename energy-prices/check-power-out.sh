f8e3cf3a5e09ee14c0563c807d696f3abf946bbe
jimo42 <mothejzik@email.cz>
2026-07-15 00:55:04 +0200

    update

--- energy-prices/check-power-out.sh
+++ energy-prices/check-power-out.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR" || exit 1
 
 PYSCRIPTS_DIR="$SCRIPT_DIR/../goodwe"
 
 #Threshold, pri krerem se nam nevyplati prodavat
-THRESHOLD="22"
+THRESHOLD="20"
 
 enable_grid_output(){
 	if [[ `$PYSCRIPTS_DIR/manage_goodwe.py read_export_limit | grep ENABLED | wc -l` -eq 1 ]] ; then 
@@ -35,8 +35,10 @@ disable_grid_output(){
 
 
 DATE_NOW=$(date "+%Y-%m-%d")
-HOUR_NOW=$(date +"%H:%M")
-#[[ $HOUR_NOW -eq 0 ]] && HOUR_NOW=24
+#HOUR_NOW=$(date +"%H:%M")   --- this works fine if we call the script from cron in */15, but doesn't work if called manually not at exact quarter hour, so improving:
+MIN_NOW=$(date +"%M")
+MIN_QUARTER=$(( 10#$MIN_NOW / 15 * 15 ))
+HOUR_NOW=$(date +"%H"):$(printf "%02d" "$MIN_QUARTER")
 
 # if primary file does not exist, use the secondary one
 PRICES="$DATE_NOW".csv
@@ -49,7 +51,7 @@ if [ ! -f "$PRICES" ] || [ "$(stat -c %s "$PRICES" 2>/dev/null)" -lt 1024 ]; the
 	fi
 fi
 
-PRICE_NOW=$(cat "$PRICES" | grep ^"$HOUR_NOW"';' | cut -d\; -f2 | tr \, \. )
+PRICE_NOW=$(cat "$PRICES" | grep ^"$HOUR_NOW"';' | cut -d\; -f2 | tr \, \. | tr -d '\r')
 
 echo -n "`date` - "
 
